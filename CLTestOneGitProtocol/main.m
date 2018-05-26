df649b2dbf09caed49154299135daa0ed573cf9d
D8Ge <burnedfrog@163.com>
2018-05-26 15:46:55 +0800

    Initial Commit

---
+++ CLTestOneGitProtocol/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CLTestOneGitProtocol
+//
+//  Created by yeyongping on 2018/5/26.
+//  Copyright © 2018年 wanchangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
